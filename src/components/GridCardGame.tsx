604e14fca2860500f7bd9d1b1fbffd78a888db7c
farah malla <farahmalla.fofo@gmail.com>
2025-01-23 16:02:55 +0300

    Create a custom hook to fetch games

--- src/components/GridCardGame.tsx
+++ src/components/GridCardGame.tsx
@@ -1,38 +1,18 @@
-import { useEffect, useState } from 'react';
-import apiClient from '../services/api-client';
 import { Text } from '@chakra-ui/react';
+import useGames from '../hooks/useGames';
 
-interface Game{
-    id: number;
-    name: string;
-}
-interface FetchGamesResponse{
-    count: number;
-    results: Game[];
-}
 const GridCardGame = () => {
-    const [ games, setGames ] = useState<Game[]>([]);
-    const [ error, setError] = useState([]);
-
-    useEffect(() =>{
-        apiClient.get<FetchGamesResponse>('/games')
-        .then((res) =>{
-            setGames(res.data.results)
-        })
-        .catch(error =>{
-            setError(error.message)
-        })
-    },[])
-  return (
-    <>
-        {error && <Text>{error}</Text>}
-        <ul>
-            {games.map((game)=>(
-                <li key={game.id}>{game.name}</li>
-            ))}
-        </ul>
-    </>
-  )
+    const { games, error} = useGames();
+    return (
+        <>
+            {error && <Text>{error}</Text>}
+            <ul>
+                {games.map((game) => (
+                    <li key={game.id}>{game.name}</li>
+                ))}
+            </ul>
+        </>
+    )
 }
 
-export default GridCardGame
+export default GridCardGame;
